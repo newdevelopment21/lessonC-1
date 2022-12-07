ef898c184fbd6df69fe7d2f4848c75f615e56019
Nitsetskaya Svetlana Aleksandrovna <newdevelopment.sa44@gmail.com>
2022-12-08 01:24:48 +0300

    Cоздана программа приветствующая особого пользователя по особенному, а обычного просто

---
+++ examples005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя пользователя: ");
+string username = Console.ReadLine();
+
+
+if (username == "Маша")
+{
+    Console.WriteLine ("Ура! Это же Маша");
+    }
+else 
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine(username);}
