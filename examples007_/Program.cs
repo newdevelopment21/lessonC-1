7f4bf0f9b90b010f1ee198dc2ef57f44a0730d02
Nitsetskaya Svetlana Aleksandrovna <newdevelopment.sa44@gmail.com>
2022-12-08 02:51:01 +0300

    проект   по нахождению точек в разработке, частично сделан

---
+++ examples007_/Program.cs
@@ -0,0 +1,12 @@
+﻿int xa = 40, ya = 1,
+    xb = 1,  yb = 1,
+    xc = 80, yc = 30;
+
+Console.SetCursorePosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorePosition(xb, yb);
+Console.WriteLine("+");
+
+Console.(xc, yc);
+ConsolSetCursorePositione.WriteLine("+");
